8ef44afbeab8b24e0a090c29311bb09774dce81a
User-425 <user425.itsme@gmail.com>
2024-05-07 18:33:42 +0700

    add: index column

--- public/js/demo/datatables-demo.js
+++ public/js/demo/datatables-demo.js
@@ -1,4 +1,17 @@
-// Call the dataTables jQuery plugin
 $(document).ready(function() {
-  $('#dataTable').DataTable();
-});
+  var t = $('#dataTable').DataTable( {
+      "columnDefs": [ {
+          "searchable": false,
+          "orderable": false,
+          "targets": 0
+      } ],
+      "order": [[ 1, 'asc' ]]
+  } );
+              
+  t.on( 'draw.dt', function () {
+  var PageInfo = $('#dataTable').DataTable().page.info();
+       t.column(0, { page: 'current' }).nodes().each( function (cell, i) {
+          cell.innerHTML = i + 1 + PageInfo.start;
+      } );
+  } );
+} );
