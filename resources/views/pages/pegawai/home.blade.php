ee4315e93b32ae9f9e4b8fe9665e984dd93e3a03
User-425 <user425.itsme@gmail.com>
2024-05-08 14:48:41 +0700

    add: template js

--- resources/views/pages/pegawai/home.blade.php
+++ resources/views/pages/pegawai/home.blade.php
@@ -330,3 +330,12 @@ Dashboard
                         </div>
                     </div>
 @endsection
+
+@section('page_script')
+    <!-- Page level plugins -->
+    <script src="vendor/chart.js/Chart.min.js"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="js/demo/chart-area-demo.js"></script>
+    <script src="js/demo/chart-pie-demo.js"></script>
+@endsection
